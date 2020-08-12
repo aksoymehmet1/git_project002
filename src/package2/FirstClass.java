0e3d3fc73c5f4c1d343489a67b86aec2626eede8
Mehmet <mayselaksoy@gmail.com>
2020-08-12 16:12:23 -0400

    second commit from local

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -5,7 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("this is the first changes from local master");
-
+		System.out.println("this is second commit from local master");
 	}
 
 }
