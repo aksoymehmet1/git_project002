c638a1bc79a7f096488471923b8f69f0aa81cd6e
Mehmet <mayselaksoy@gmail.com>
2020-08-12 16:16:52 -0400

    third commit from local

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -6,6 +6,7 @@ public class FirstClass {
 		
 		System.out.println("this is the first changes from local master");
 		System.out.println("this is second commit from local master");
+		System.out.println("this is third commit from local master");
 	}
 
 }
